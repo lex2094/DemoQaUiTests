6f626ba24b2da1d9237c32c6118678556518a8dd
lex2094 <alesha1240@mail.ru>
2023-01-07 16:57:20 +0800

    removed old tests

---
+++ src/test/java/com/demoqa/pages/registrationform/components/ResultsModal.java
@@ -0,0 +1,22 @@
+package com.demoqa.pages.registrationform.components;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ResultsModal {
+
+    private final static String TITLE_TEXT = "Thanks for submitting the form";
+
+    public ResultsModal checkVisible() {
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text(TITLE_TEXT));
+
+        return this;
+    }
+    public ResultsModal checkResult(String key, String value) {
+        $(".table-responsive table").$(byText(key))
+                .parent().shouldHave(text(value));
+        return this;
+    }
+}
---
+++ src/test/java/com/demoqa/tests/RegistrationFormWithPageObjectsUtilsTests.java
@@ -0,0 +1,36 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.registrationform.RegistrationFormPage;
+import com.demoqa.pages.registrationform.components.ResultsModal;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationFormWithPageObjectsUtilsTests extends TestBase {
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    ResultsModal resultsModal = new ResultsModal();
+
+
+    @Test
+    void fillFormTest() {
+        registrationFormPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender("Other")
+                .setNumber(phone)
+                .setBirthDate(generateDay(), generateMonth(), generateYear())
+                .setSubjectInput()
+                .setHobbiesWrapper()
+                .uploadPicture()
+                .setCurrentAddress()
+                .setState()
+                .setCity();
+        registrationFormPage.clickSubmit();
+        resultsModal.checkVisible();
+        registrationFormPage.checkResultsTableVisible()
+                    .checkResult("Student Name", firstName)
+                    .checkResult("Student Email", email)
+                    .checkResult("Date of Birth", generateDay() + " " + generateMonth() + "," + generateYear());
+        resultsModal.checkVisible();
+        System.out.println("okk");
+    }
+}
